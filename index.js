bb4528c1f0dd616b35460734a5d2b9630349d1eb
Tougrel <lambrosagris@gmail.com>
2024-07-07 10:55:23 +0300

    feat: added an option to toggle between ban/kick mode

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
-import { Client, ActivityType, GatewayIntentBits, Partials, GuildMemberFlags, Events } from "discord.js";
-import { readFile, writeFile } from "node:fs/promises";
+import {GatewayIntentBits, ActivityType, GuildMemberFlags} from "discord-api-types/v10";
+import {Client, Partials, Events, GuildMember} from "discord.js";
+import {readFile, writeFile} from "node:fs/promises";
 
 const client = new Client({
     partials: [Partials.User, Partials.GuildMember],
@@ -21,29 +22,55 @@ client.once(Events.ClientReady, () => {
 
 client.on(Events.GuildMemberAdd, async (member) => {
     // Get the config
-    const config = JSON.parse(await readFile("configs/config.json"));
+    const config = JSON.parse((await readFile("configs/config.json")).toString());
 
     // If the system is not enabled, the member has the bypass verification flag and the member is not kickable then stop here
-    if (!config.kick || member.flags.has(GuildMemberFlags.BypassesVerification) || !member.kickable) return;
+    if (!config.enabled || member.user.bot || member.flags.has(GuildMemberFlags.BypassesVerification) || !member.kickable) return;
 
-    // Kick the member
-    member.kick("Automatic kick system");
+    // If the mode in the config is set to kick then kick the user
+    if (config.mode === "kick") await member.kick("Yunya lockdown system");
+    // else just drop the ban hammer on him :D
+    else await member.ban({reason: "Yunya lockdown system"});
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
     if (interaction.isChatInputCommand()) {
         if (interaction.commandName === "yunya") {
-            const toggle = interaction.options.getBoolean("value", true);
-            const config = JSON.parse(await readFile("configs/config.json"));
+            if (interaction.options.getSubcommand() === "toggle") {
+                const toggle = interaction.options.getBoolean("value", true);
+                const config = JSON.parse((await readFile("configs/config.json")).toString());
 
-            config.kick = toggle;
-            await writeFile("configs/config.json", JSON.stringify(config, null, 4), "utf-8");
+                config.enabled = toggle;
+                await writeFile("configs/config.json", JSON.stringify(config, null, 4), "utf-8");
+                await interaction.reply({
+                    ephemeral: true,
+                    content: `✅ Successfully ${config.kick ? "enabled" : "disabled"} the system!`,
+                });
+
+                return;
+            } else if (interaction.options.getSubcommand() === "mode") {
+                const mode = interaction.options.getString("value", true);
+                const config = JSON.parse((await readFile("configs/config.json")).toString());
+
+                config.mode = mode;
+                await writeFile("configs/config.json", JSON.stringify(config, null, 4), "utf-8");
+                await interaction.reply({
+                    ephemeral: true,
+                    content: `✅ Successfully changed the lockdown mode to ${mode}!`
+                });
+
+                return;
+            }
+        }
+
+        if (interaction.commandName === "bite") {
+            const member = interaction.options.getUser("pestie");
+
             await interaction.reply({
-                ephemeral: true,
-                content: `✅ Successfully ${config.kick ? "enabled" : "disabled"} the system!`,
+                content: `${client.user} attacks ${member.username}! <:PestoFood:1075882159115612252>`,
             });
         }
     }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NODE_ENV === "development" ? process.env.DEV_BOT_TOKEN : process.env.BOT_TOKEN).catch(console.error);
