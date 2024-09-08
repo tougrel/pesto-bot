862facfe7cc5dd2d5858dfb456e7216cbcb0c980
Tougrel <lambrosagris@gmail.com>
2024-09-08 15:15:53 +0300

    feat(ppcheck): new special message and power adjustment

--- index.js
+++ index.js
@@ -72,17 +72,18 @@ client.on(Events.InteractionCreate, async (interaction) => {
 		}
 		
 		if (interaction.commandName === "ppcheck") {
-			const power = Math.floor(Math.random() * 100);
+			const power = Math.floor(Math.random() * 101);
 			
 			let message;
-			if (power === 69) message = "nice... <:yuniiGasm:1281945197655363655>";
+			if (power === 0) message = "non existent! <:ppcheck:1282311070924673050>";
+			else if (power === 69) message = "nice... <:yuniiGasm:1281945197655363655>";
 			else if (power <= 20) message = "So smol, can't even see it! <:yuniiWut:1281948023819337812>";
 			else if (power <= 50) message = "hmm... decent size! <:yuniiPog:1281948035181711400>";
 			else if (power <= 80) message = "Beeg! Nice! <:yuniiUwaa:1281948029431316530>";
 			else message = "Enormous, Gigantic! Overflowing with pesto! <:yuniiCultured:1281948041032765490>"
 			
 			await interaction.reply({
-				content: `${interaction.user}'s Pesto Power is ${power} ${message}`,
+				content: `${interaction.user}'s Pesto Power is **${power}%**, ${message}`,
 			});
 		}
 	}
