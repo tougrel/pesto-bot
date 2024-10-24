c72c27b40354a138fba104d25dca9b97d0623fda
Tougrel <lambrosagris@gmail.com>
2024-10-24 15:46:02 +0300

    feat(commands): new messages, boosted weekends and negative ppcheck

--- commands/ppcheck.js
+++ commands/ppcheck.js
@@ -1,15 +1,51 @@
+const MESSAGES = {
+	"-100": ["Turned into dust! Disappeared from the face of the universe! <:ppcheck:1282311070924673050>"],
+	"-99": ["Turns out you are a potato since you keep growing downward <a:smoking:1298958916452876288>"],
+	"-80": ["You are not even worthy being food <:yuniiJail:1298954463373168660>"],
+	"-50": ["Turns out radiation DONT make things grow <:pestoSlime:1298958398577971220>"],
+	"-20": ["it can get this small? <:pestoGaspW:1137804322684547144>"],
+	"-69":  ["reverse nice?... <:yuniiGasm:1281945197655363655>"],
+	"0": ["non existent! Can't even find it with a magnifying glass <:pestoDetective:1298959707888812042>", "Congratulations, you reached absolute zero (which is -273.15 degrees Celsius)"],
+	"69": ["nice... <:yuniiGasm:1281945197655363655>"],
+	"20": ["So smol, can't even see it! <:yuniiWut:1281948023819337812>"],
+	"50": ["hmm... decent size! <:yuniiPog:1281948035181711400>"],
+	"80": ["Beeg! Nice! <:yuniiUwaa:1281948029431316530>"],
+	"99": ["Enormous, Gigantic! Overflowing with pesto! <:yuniiCultured:1281948041032765490>"],
+	"100": ["<:yuniiuh:1298954249908125747>"],
+}
+
 export const name = "ppcheck";
 export async function run(client, interaction) {
 	const user = interaction.options.getUser("pestie", false);
-	const power = Math.floor(Math.random() * 101);
+	let power = Math.floor(Math.random() * 101);
 	
 	let message;
-	if (power === 0) message = "non existent! <:ppcheck:1282311070924673050>";
-	else if (power === 69) message = "nice... <:yuniiGasm:1281945197655363655>";
-	else if (power <= 20) message = "So smol, can't even see it! <:yuniiWut:1281948023819337812>";
-	else if (power <= 50) message = "hmm... decent size! <:yuniiPog:1281948035181711400>";
-	else if (power <= 80) message = "Beeg! Nice! <:yuniiUwaa:1281948029431316530>";
-	else message = "Enormous, Gigantic! Overflowing with pesto! <:yuniiCultured:1281948041032765490>"
+	if (power === 0) message = MESSAGES["0"][Math.floor(Math.random() * MESSAGES["0"].length)];
+	else if (power === 69) message = MESSAGES["69"][Math.floor(Math.random() * MESSAGES["69"].length)];
+	else if (power === 100) message = MESSAGES["100"][Math.floor(Math.random() * MESSAGES["100"].length)];
+	else if (power <= 20) message = MESSAGES["20"][Math.floor(Math.random() * MESSAGES["20"].length)];
+	else if (power <= 50) message = MESSAGES["50"][Math.floor(Math.random() * MESSAGES["50"].length)];
+	else if (power <= 80) message = MESSAGES["80"][Math.floor(Math.random() * MESSAGES["80"].length)];
+	else message = MESSAGES["99"][Math.floor(Math.random() * MESSAGES["99"].length)];
+	
+	const date = new Date();
+	const day = date.getUTCDay();
+	if (day === 0 || day === 6) {
+		power = Math.floor(Math.random() * 101) + 35;
+		
+		const random = Math.random().toFixed(2);
+		if (random <= 0.1) {
+			power = Math.floor(Math.random() * 101) - 100;
+			
+			if (power === 0) message = MESSAGES["0"][Math.floor(Math.random() * MESSAGES["0"].length)];
+			else if (power === -69) message = MESSAGES["-69"][Math.floor(Math.random() * MESSAGES["-69"].length)];
+			else if (power === -100) message = MESSAGES["-100"][Math.floor(Math.random() * MESSAGES["-100"].length)];
+			else if (-80 > power) message = MESSAGES["-99"][Math.floor(Math.random() * MESSAGES["-99"].length)];
+			else if (-50 > power) message = MESSAGES["-80"][Math.floor(Math.random() * MESSAGES["-80"].length)];
+			else if (-20 > power) message = MESSAGES["-50"][Math.floor(Math.random() * MESSAGES["-50"].length)];
+			else if (0 > power) message = MESSAGES["-20"][Math.floor(Math.random() * MESSAGES["-20"].length)];
+		}
+	}
 	
 	if (user) {
 		const member = interaction.guild.members.cache.get(user.id);
