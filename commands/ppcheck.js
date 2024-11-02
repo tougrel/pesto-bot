743f23c662d445ad16cf40a241aab37cd243ad48
Tougrel <lambrosagris@gmail.com>
2024-11-02 22:51:45 +0200

    refactor(commands): remove unused import

--- commands/ppcheck.js
+++ commands/ppcheck.js
@@ -1,12 +1,10 @@
-import {Collection} from "discord.js";
-
 const MESSAGES = {
 	"-100": ["Turned into dust! Disappeared from the face of the universe! <:ppcheck:1282311070924673050>"],
 	"-99": ["Turns out you are a potato since you keep growing downward <a:smoking:1298958916452876288>"],
 	"-80": ["You are not even worthy being food <:yuniiJail:1298954463373168660>"],
 	"-50": ["Turns out radiation DONT make things grow <:pestoSlime:1298958398577971220>"],
 	"-20": ["it can get this small? <:pestoGaspW:1137804322684547144>"],
-	"-69":  ["reverse nice?... <:yuniiGasm:1281945197655363655>"],
+	"-69": ["reverse nice?... <:yuniiGasm:1281945197655363655>"],
 	"0": ["non existent! Can't even find it with a magnifying glass <:pestoDetective:1298959707888812042>", "Congratulations, you reached absolute zero (which is -273.15 degrees Celsius)"],
 	"69": ["nice... <:yuniiGasm:1281945197655363655>"],
 	"20": ["So smol, can't even see it! <:yuniiWut:1281948023819337812>"],
