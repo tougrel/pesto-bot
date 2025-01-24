e449063bf8bdc8883133a2865042a9a054b80637
Tougrel <lambrosagris@gmail.com>
2025-01-24 21:01:47 +0200

    feat(commands): add user option on clueless and copium commands so they behave similar to ppcheck

--- commands/clueless.js
+++ commands/clueless.js
@@ -1,6 +1,26 @@
 export const name = "clueless";
 export async function run(client, interaction) {
 	const db = client.database;
+	const user = interaction.options.getUser("pestie", false);
+	if (user && user.id !== interaction.user.id) {
+		const [rows] = await db.query(db.format("SELECT power FROM Clueless WHERE user_id = ? AND expires >= ?", [user.id, Date.now()]));
+
+		if (rows.length > 0) {
+			const power = rows[0].power;
+			await interaction.reply({
+				content: `**${user}'s** cluelessness was **${power}%** today! ${user.id === "236642620506374145" ? "<:cluelessKing:1332416626251010153> <:pestoBow:1332418781133410446>" : ""}`,
+				ephemeral: true
+			});
+		} else {
+			await interaction.reply({
+				content: "I couldn't find any data for this pestie <:yuniiLost:1329480382843850815> It looks like they didn't do their clueless check today! <a:madPesto:1329480709328343132>",
+				ephemeral: true
+			});
+		}
+
+		return;
+	}
+
 	const [rows] = await db.query(db.format("SELECT expires FROM Clueless WHERE user_id = ? AND expires >= ?", [interaction.user.id, Date.now()]));
 	if (rows.length > 0) {
 		const timestamp = Math.round(rows[0].expires / 1000);
@@ -12,14 +32,18 @@ export async function run(client, interaction) {
 		return;
 	}
 	
-	let power = Math.floor(Math.random() * 101);
 	const date = getExpireTimestamp();
+	let power = Math.floor(Math.random() * 101);
+	let emoji = false;
 
 	// Here we generate the power for our clueless king Aleg with a minimum of 100!
-	if (interaction.user.id === "236642620506374145") power = Math.floor(Math.random() * (10000 - 100)) + 100;
+	if (interaction.user.id === "236642620506374145") {
+		power = Math.floor(Math.random() * (10000 - 100)) + 100;
+		emoji = true;
+	}
 	
 	await interaction.reply({
-		content: `${interaction.user}'s cluelessness is **${power}%** today!`
+		content: `${interaction.user}'s cluelessness is **${power}%** today! ${emoji ? "<:cluelessKing:1332416626251010153> <:pestoBow:1332418781133410446>" : ""}`
 	});
 
 	await db.query(db.format("INSERT INTO Clueless(user_id, power, expires) VALUES(?, ?, ?)", [interaction.user.id, power, date]));
--- commands/copium.js
+++ commands/copium.js
@@ -1,6 +1,26 @@
 export const name = "copium";
 export async function run(client, interaction) {
 	const db = client.database;
+	const user = interaction.options.getUser("pestie", false);
+	if (user && user.id !== interaction.user.id) {
+		const [rows] = await db.query(db.format("SELECT power FROM Copium WHERE user_id = ? AND expires >= ?", [user.id, Date.now()]));
+
+		if (rows.length > 0) {
+			const power = rows[0].power;
+			await interaction.reply({
+				content: `**${user}'s** copium level was **${power}%** today! ${user.id === "124963012321738752" ? "<:copiumKing:1332416650900799619> <:pestoBow:1332418781133410446>" : ""}`,
+				ephemeral: true
+			});
+		} else {
+			await interaction.reply({
+				content: "I couldn't find any data for this pestie <:yuniiLost:1329480382843850815> It looks like they didn't do their copium check today! <a:madPesto:1329480709328343132>",
+				ephemeral: true
+			});
+		}
+
+		return;
+	}
+
 	const [rows] = await db.query(db.format("SELECT expires FROM Copium WHERE user_id = ? AND expires >= ?", [interaction.user.id, Date.now()]));
 	if (rows.length > 0) {
 		const timestamp = Math.round(rows[0].expires / 1000);
@@ -12,14 +32,18 @@ export async function run(client, interaction) {
 		return;
 	}
 
-	let power = Math.floor(Math.random() * 101);
 	const date = getExpireTimestamp();
+	let power = Math.floor(Math.random() * 101);
+	let emoji = false;
 
 	// Here we generate the power for our copiun king Warlord with a minimum of 100!
-	if (interaction.user.id === "124963012321738752") power = Math.floor(Math.random() * (10000 - 100)) + 100;
+	if (interaction.user.id === "124963012321738752") {
+		power = Math.floor(Math.random() * (10000 - 100)) + 100;
+		emoji = true;
+	}
 
 	await interaction.reply({
-		content: `${interaction.user}'s copium level is **${power}%** today!`
+		content: `${interaction.user}'s copium level is **${power}%** today! ${emoji ? "<:copiumKing:1332416650900799619> <:pestoBow:1332418781133410446>" : ""}`
 	});
 
 	await db.query(db.format("INSERT INTO Copium(user_id, power, expires) VALUES(?, ?, ?)", [interaction.user.id, power, date]));
