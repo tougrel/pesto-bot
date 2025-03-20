c68d1602923bf83082af59afc98746de89e48ee1
Tougrel <lambrosagris@gmail.com>
2025-03-20 16:37:11 +0200

    fix(utils): swap discord ids cause I can't test simple stuff

--- utils/checks.js
+++ utils/checks.js
@@ -2,13 +2,13 @@
  * @param id { String } the Discord ID of a user
  */
 export function checkCluelessKing(id) {
-    return id === "124963012321738752";
+    return id === "236642620506374145";
 }
 
 /**
  * @param id { String } the Discord ID of a user
  */
 export function checkCopiumKing(id) {
-    return id === "236642620506374145";
+    return id === "124963012321738752";
 }
 
