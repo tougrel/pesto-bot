30a5946bdb4c7953ba6e9d3861a28e7364812b6a
Tougrel <lambrosagris@gmail.com>
2025-05-04 02:42:16 +0300

    feat: new messages for ppcheck

--- utils/messages.js
+++ utils/messages.js
@@ -1,17 +1,56 @@
 export const PPCHECK_MESSAGES = {
-	"-100": ["Turned into dust! Disappeared from the face of the universe! <:ppcheck:1282311070924673050>"],
-	"-99": ["Turns out you are a potato since you keep growing downward <a:smoking:1298958916452876288>"],
-	"-80": ["You are not even worthy being food <:yuniiJail:1298954463373168660>"],
-	"-50": ["Turns out radiation DONT make things grow <:pestoSlime:1298958398577971220>"],
-	"-20": ["it can get this small? <:pestoGaspW:1137804322684547144>"],
-	"-69": ["reverse nice?... <:yuniiGasm:1281945197655363655>"],
-	"0": ["non existent! Can't even find it with a magnifying glass <:pestoDetective:1298959707888812042>", "Congratulations, you reached absolute zero (which is -273.15 degrees Celsius)"],
-	"69": ["nice... <:yuniiGasm:1281945197655363655>"],
-	"20": ["So smol, can't even see it! <:yuniiWut:1281948023819337812>"],
-	"50": ["hmm... decent size! <:yuniiPog:1281948035181711400>"],
-	"80": ["Beeg! Nice! <:yuniiUwaa:1281948029431316530>"],
-	"99": ["Enormous, Gigantic! Overflowing with pesto! <:yuniiCultured:1281948041032765490>"],
-	"100": ["<:yuniiuh:1298954249908125747>"],
+	"-100": [
+		"Turned into dust! Disappeared from the face of the universe! <:ppcheck:1282311070924673050>",
+		"Scientists need a quantum microscope to study this phenomenon",
+	],
+	"-99": [
+		"Turns out you are a potato since you keep growing downward <a:smoking:1298958916452876288>",
+		"Congrats! Your Pesto Power has officially reached the Earth's core!"
+	],
+	"-80": [
+		"You are not even worthy being food <:yuniiJail:1298954463373168660>",
+		"Scientists need a quantum microscope to study this phenomenon",
+	],
+	"-50": [
+		"Turns out radiation DONT make things grow <:pestoSlime:1298958398577971220>",
+		"It's evolving, just backwards!",
+	],
+	"-20": [
+		"it can get this small? <:pestoGaspW:1137804322684547144>",
+		"Is that a PP or just a pixel on my screen?",
+	],
+	"-69": [
+		"reverse nice?... <:yuniiGasm:1281945197655363655>",
+		"Task failed successfully! <:yuniiGasm:1281945197655363655>"
+	],
+	"0": [
+		"non existent! Can't even find it with a magnifying glass <:pestoDetective:1298959707888812042>",
+		"Congratulations, you reached absolute zero (which is -273.15 degrees Celsius)",
+	],
+	"69": [
+		"nice... <:yuniiGasm:1281945197655363655>",
+		"The legendary number has chosen you! Nice.. <:yuniiGasm:1281945197655363655>",
+	],
+	"20": [
+		"So smol, can't even see it! <:yuniiWut:1281948023819337812>",
+		"It's trying its best, okay? <:pestoSlime:1298958398577971220>",
+	],
+	"50": [
+		"hmm... decent size! <:yuniiPog:1281948035181711400>",
+		"Definitely nothing to be ashamed of!",
+	],
+	"80": [
+		"Beeg! Nice! <:yuniiUwaa:1281948029431316530>",
+		"Impressive... most impressive",
+	],
+	"99": [
+		"Enormous, Gigantic! Overflowing with pesto! <:yuniiCultured:1281948041032765490>",
+		"NASA wants to study this cosmic anomaly <:pestoDetective:1298959707888812042>",
+	],
+	"100": [
+		"<:yuniiuh:1298954249908125747>",
+		"MAXIMUM OVERDRIVE ACHIEVED!",
+	],
 }
 
 export function getPPCheckMessage(power) {
