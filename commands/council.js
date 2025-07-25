71a06bbe6a467ee203c3235ac542f6eb506fa625
Tougrel <lambrosagris@gmail.com>
2025-07-26 01:04:05 +0300

    feat: avg_power and total_rolls for special seats, user display rank and reset reminder

--- commands/council.js
+++ commands/council.js
@@ -7,7 +7,13 @@ export async function run(client, interaction) {
 	await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 	
 	const db = client.database;
-	const [rows] = await db.query(db.format("SELECT user_id, avg_power, total_rolls from TheCouncil ORDER BY avg_power DESC"));
+	const [rows] = await db.query(db.format("SELECT user_id, avg_power, total_rolls from TheCouncil"));
+	const [full] = await db.query(db.format("SELECT user_id, avg_power, total_rolls from TheCouncilFull"));
+	
+	const aleg = full.filter((row) => row.user_id === "236642620506374145");
+	const warlord = full.filter((row) => row.user_id === "124963012321738752");
+	const user = full.filter((row) => row.user_id === interaction.user.id);
+	const userIndex = full.findIndex((row) => row.user_id === interaction.user.id);
 	
 	const council_emote = await client.application.emojis.fetch("1398244533950480534");
 	await interaction.editReply({
@@ -21,16 +27,16 @@ export async function run(client, interaction) {
 						components: [
 							{
 								type: ComponentType.TextDisplay,
-								content: "# The Council",
+								content: `# The Council\n## Special seats\n1. <@236642620506374145> the clueless king ${aleg.length > 0 ? `with an average **${aleg[0].avg_power}** power and **${aleg[0].total_rolls}** rolls` : ""} <:cluelessKing:1332416626251010153> <:pestoBow:1332418781133410446>\n2. <@124963012321738752> the copium king ${warlord.length > 0 ? `with an average **${warlord[0].avg_power}** power and **${warlord[0].total_rolls}** rolls` : ""} <:copiumKing:1332416650900799619> <:pestoBow:1332418781133410446>`,
 							},
 							{
 								type: ComponentType.TextDisplay,
-								content: "Special seats:\n1. <@236642620506374145> the clueless king <:cluelessKing:1332416626251010153> <:pestoBow:1332418781133410446>\n2. <@124963012321738752> the copium king <:copiumKing:1332416650900799619> <:pestoBow:1332418781133410446>",
+								content: `## Elected Members\n${rows.map((row, index) => `${index}. <@${row.user_id}> with an average **${row.avg_power}** power and **${row.total_rolls}** rolls`).join("\n")}`,
 							},
 							{
 								type: ComponentType.TextDisplay,
-								content: rows.map((row, index) => `${index}. <@${row.user_id}> with an average **${row.avg_power}** power and **${row.total_rolls}** rolls`).join("\n"),
-							},
+								content: `${user.length > 0 ? `### Your Rank is **${userIndex + 1}** with an average power of **${user[0].avg_power}**\n` : ""}-# The Council elected members reset every month!`
+							}
 						],
 						accessory: {
 							type: ComponentType.Thumbnail,
