3d3a8f5eb8b4441dc03f7b03c2ea4d7f3aa6ed2a
Lampros Sagris <lambrosagris@gmail.com>
2026-06-04 13:47:12 +0300

    refactor: clueless/copiun king checks and responses

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.database = pool;
 
 client.once(Events.ClientReady, async () => {
 	console.log("Nya");
-	
+
 	const files = await readdir("./commands/");
 	for (const file of files) {
 		const command = await import((`./commands/${file}`));
@@ -43,10 +43,10 @@ client.once(Events.ClientReady, async () => {
 client.on(Events.GuildMemberAdd, async (member) => {
 	// Get the config
 	const config = JSON.parse((await readFile("configs/config.json")).toString());
-	
+
 	// If the system is not enabled, the member has the bypass verification flag and the member is not kickable then stop here
 	if (!config.enabled || member.user.bot || member.flags.has(GuildMemberFlags.BypassesVerification) || !member.kickable) return;
-	
+
 	// If the mode in the config is set to kick then kick the user
 	if (config.mode === "kick") await member.kick("Yunya lockdown system");
 	// else just drop the ban hammer on him :D
@@ -61,44 +61,70 @@ client.on(Events.MessageCreate, async (message) => {
 			content: "<:blameMerry1:1404817851919110264><:blameMerry2:1404817860773150762>",
 		});
 	}
-	
+
 	if (checkBanker(message.author.id.toString()) || message.author.id === process.env.DEVELOPER_DISCORD_ID) {
 		if (message.content.startsWith(`<@${client.user.id}> set bank fee to`)) {
 			const args = message.content.split(" ");
 			if (isNaN(parseInt(args[5].replace("%", "")))) {
 				return;
 			}
-			
+
 			const number = parseInt(args[5].replace(/%/, ""));
 			await message.reply({
 				content: `Setting bank fee to **${number}**%`,
 			});
 		}
 	}
-	
+
 	if (checkCluelessKing(message.author.id.toString())) {
-		if (/not (.+)? clueless (king)?/gi.test(message.content)) {
-			await message.reply({
-				content: "<:cluelessKing:1332416626251010153> <:pestobow:1332418781133410446>"
-			});
-		}
-		else if (/(.+)?I|1(.+)?/gi.test(message.content)) {
-//			TODO: make this less frequent and less annoying! (maybe add a chance like the weekend ppcheck?)
-//			await message.react("1332416626251010153");
-//			await message.react("1332418781133410446");
+		if (/not(.+)?clueless(.+)?(king)??/gi.test(message.content)) {
+      const random = Math.random();
+			console.debug('[cluelessKing] not clueless check', random);
+			if (random < 0.25) {
+        const moreRandomStuffCauseWhyNot = Math.random();
+				console.debug('[cluelessKing] not clueless check 2', moreRandomStuffCauseWhyNot);
+				if (moreRandomStuffCauseWhyNot < 0.5) {
+					await message.reply({
+						content: "<:cluelessKing:1332416626251010153> <:pestobow:1332418781133410446>"
+					});
+				} else {
+					await message.react("1332416626251010153");
+					await message.react("1332418781133410446");
+				}
+			}
 		}
 	}
 
 	if (checkCopiumKing(message.author.id.toString())) {
-		if (/not (.+)? copium (king)?/gi.test(message.content)) {
-			await message.reply({
-				content: "<:copiumKing:1332416650900799619> <:pestobow:1332418781133410446>"
-			});
-		}
-		else if (/(.+)?I(.+)?/gi.test(message.content)) {
-//			TODO: make this less frequent and less annoying! (maybe add a chance like the weekend ppcheck?)
-//			await message.react("1332416650900799619");
-//			await message.react("1332418781133410446");
+		if (/not(.+)?copium(.+)?(king)??/gi.test(message.content)) {
+      const random = Math.random();
+			console.debug('[copiumKing] not copium check', random);
+			if (random < 0.25) {
+        const moreRandomStuffCauseWhyNot = Math.random();
+				console.debug('[copiumKing] not copium check 2', moreRandomStuffCauseWhyNot);
+				if (moreRandomStuffCauseWhyNot < 0.5) {
+					await message.reply({
+						content: "<:copiumKing:1332416650900799619> <:pestobow:1332418781133410446>"
+					});
+				} else {
+					await message.react("1332416650900799619");
+					await message.react("1332418781133410446");
+				}
+			}
+		} else if (message.content.includes('rigged')) {
+		  const random = Math.random();
+			console.debug('[copiumKing] rigged', random);
+			if (random < 0.25) {
+			  await message.reply({
+				  content: '<:lookDownPestie:1512044054471245854>',
+					files: [
+					  {
+							attachment: 'https://cdn.pestoverse.world/yunya/warlord.png',
+							name: 'warlord-rigged.png',
+						},
+					],
+				});
+			}
 		}
 	}
 });
@@ -107,7 +133,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
 	if (interaction.isChatInputCommand()) {
 		if (commands.has(interaction.commandName)) {
 			const command = commands.get(interaction.commandName);
-			
+
 			if (process.env.MAINTENANCE === "true" && (interaction.user.id !== process.env.DEVELOPER_DISCORD_ID && interaction.user.id !== "682284810030415903" && interaction.user.id !== "212975234427518979")) {
 				return await interaction.reply({
 					content: "Under maintenance!",
