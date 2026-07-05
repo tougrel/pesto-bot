8f0b9c5ece503099d293692abf2424943662f97e
Lampros Sagris <lambrosagris@gmail.com>
2026-07-05 21:38:21 +0300

    fix: eno user option

--- app/commands/eno.ts
+++ app/commands/eno.ts
@@ -3,8 +3,8 @@ import { defineCommand } from "@lib";
 export default defineCommand({
     name: "eno",
     run: async (client, interaction) => {
-        const user = interaction.options.getUser("user", false) || interaction.user;
-        
+        const user = interaction.options.getUser("pestie", false) || interaction.user;
+
         await interaction.reply({
             content: `${user.username} English Only Apenas inglês, Solo inglese, 英語だけ, Solo inglés, 영어 만, Только английский, ENGLISH ONLY <:a:yuyuRage:1523394858645983404>`,
         });
