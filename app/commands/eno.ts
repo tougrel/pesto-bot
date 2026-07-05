72d36cdb9dc3bd1505da49cb090085a498134298
Lampros Sagris <lambrosagris@gmail.com>
2026-07-05 21:39:05 +0300

    fix: eno emote

--- app/commands/eno.ts
+++ app/commands/eno.ts
@@ -6,7 +6,7 @@ export default defineCommand({
         const user = interaction.options.getUser("pestie", false) || interaction.user;
 
         await interaction.reply({
-            content: `${user.username} English Only Apenas inglês, Solo inglese, 英語だけ, Solo inglés, 영어 만, Только английский, ENGLISH ONLY <:a:yuyuRage:1523394858645983404>`,
+            content: `${user.username} English Only Apenas inglês, Solo inglese, 英語だけ, Solo inglés, 영어 만, Только английский, ENGLISH ONLY <a:yuyuRage:1523394858645983404>`,
         });
     }
 });
