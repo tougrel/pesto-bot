3448a789031e18419b868b873709e5f2d337e8af
b0xLUL <djslememeel@gmail.com>
2026-07-12 18:43:28 +0200

    made interface for emotes and errors

---
+++ app/utils/errors.ts
@@ -0,0 +1,6 @@
+export const GambaErrors = {
+    GAME_NOT_FOUND: new Error("GAME_NOT_FOUND"),
+    GAME_ALREADY_PRESENT: new Error(),
+    PESTIE_NOT_FOUND: new Error(),
+    PESTIE_ALREADY_ADDED: new Error(),
+}
