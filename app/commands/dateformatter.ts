f9bfd5268bd6967f6dfbfc26dd43f662221ec390
b0xLUL <djslememeel@gmail.com>
2026-08-19 16:05:06 +0200

    why am i doing this pt.2 feat dippes idea

--- app/commands/dateformatter.ts
+++ app/commands/dateformatter.ts
@@ -12,6 +12,7 @@ export default defineCommand({
         // Date input should be YYYY-MM-DD
         const dateInput = interaction.options.getString("date", true)
         const zeroes = interaction.options.getBoolean("use-zeroes", false) || true
+        const lowToHigh = interaction.options.getBoolean("number-sorting", false) || false
 
         const [year, month, day] = dateInput.split("-");
 
@@ -23,23 +24,20 @@ export default defineCommand({
             return;
         }
 
-        
+        //format to YMYDYDYM
+        let dateOutput = `${year[0]}${month[0]}${year[1]}${day[0]}${year[2]}${day[1]}${year[3]}${month[1]}`
 
-        if(zeroes) {
-            //format to YMYDYDYM
-            const dateOutput = `${year[0]}${month[0]}${year[1]}${day[0]}${year[2]}${day[1]}${year[3]}${month[1]}`
-
-            interaction.editReply({
-                content: `I don't know why you would want this, but here you go: ${dateOutput}`
-            })
-        } else if (!zeroes) {
-            const dateOutput = `${year[0]}${month[0]}${year[1]}${day[0]}${year[2]}${day[1]}${year[3]}${month[1]}`.replaceAll("0","")
-
-            interaction.editReply({
-                content: `I don't know why you would want this, but here you go: ${dateOutput}`
-            })
+        if (!zeroes) {
+            dateOutput = dateOutput.replaceAll("0","")
         }
 
+        if (lowToHigh) {
+            dateOutput = Array.from(dateOutput).sort().join('')
+        }
+
+        interaction.editReply({
+                content: `I don't know why you would want this, but here you go: ${dateOutput}`
+            })
         
     }
 })
